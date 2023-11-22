1692c618cf578392995cefb80bef1f60a37775a6
Fabio Prizio <fabio.prizio@casavo.com>
2023-11-22 19:25:38 +0100

    feat: pass server port as a variable

--- notedly-api/src/index.js
+++ notedly-api/src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const port = process.env.PORT || 4000
 
-app.get('/', (_req, res) => res.send('Hello world'));
-app.listen(4000, () => console.log('Listening on port 4000...'))
+app.get('/', (_req, res) => res.send('Hello world!!'));
+app.listen(port, () => console.log(`Server running at http://localhost:${port} ...`))
