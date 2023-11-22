e0357462dba64ba9e46e78b55ce7cf87a6b71e6b
Fabio Prizio <fabio.prizio@casavo.com>
2023-11-22 13:54:41 +0100

    feat: hello world

---
+++ notedly-api/src/index.js
@@ -0,0 +1,5 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (_req, res) => res.send('Hello world'));
+app.listen(4000, () => console.log('Listening on port 4000...'))
